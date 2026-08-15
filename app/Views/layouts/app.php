00374cde0a012cb43dd8e3855bd7412a2ae850c7
farid280307-art <farid280307@gmail.com>
2026-08-15 09:28:40 +0700

    Add logout confirmation modal

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -98,6 +98,35 @@ $navigationItems = $isAdminLayout
         </main>
     </div>
 
+    <div
+        class="modal fade"
+        id="logoutConfirmationModal"
+        tabindex="-1"
+        aria-labelledby="logoutConfirmationModalLabel"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content border-0 shadow">
+                <div class="modal-header border-0 pb-0">
+                    <h2 class="modal-title fs-5" id="logoutConfirmationModalLabel">
+                        Konfirmasi Logout
+                    </h2>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
+                </div>
+                <div class="modal-body pt-3">
+                    <p class="mb-0 text-secondary">Apakah Anda yakin ingin keluar dari aplikasi?</p>
+                </div>
+                <div class="modal-footer border-0 pt-0">
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Batal</button>
+                    <button type="button" class="btn btn-danger" id="logoutConfirmButton">
+                        <i class="bi bi-box-arrow-right me-1" aria-hidden="true"></i>
+                        Ya, Logout
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="<?= e(asset('js/app.js')) ?>"></script>
     <?php foreach ($pageScripts as $pageScript): ?>
